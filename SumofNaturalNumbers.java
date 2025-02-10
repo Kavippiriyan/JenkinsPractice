854daae6cceda8f836ec3c909f68e48099f9c262
Kavippiriyan <138883580+Kavippiriyan@users.noreply.github.com>
2025-02-10 17:54:42 +0530

    Create SumofNaturalNumbers.java

---
+++ SumofNaturalNumbers.java
@@ -0,0 +1,22 @@
+package BasicPrograms;
+
+import java.util.Scanner;
+
+public class sumOfNaturalNumbers {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		int num = 5;
+		int temp=0;
+		for(int i=0;i<=num;i++)
+		{
+			temp = temp+i;
+		}
+		System.out.println("Sum of Natural Numbers:"+temp);
+		
+		
+	}
+
+}
